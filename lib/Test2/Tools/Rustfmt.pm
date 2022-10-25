1513bc70ab5ba5b4555879ec67e77cb543aca863
Graham Ollis <plicease@cpan.org>
2022-10-25 05:52:56 -0600

    Initial structure

---
+++ lib/Test2/Tools/Rustfmt.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Test2::Tools::Rustfmt {
+
+  # ABSTRACT: Test that bundled Rust code is formatted according to Rust style guidelines
+}
+
+1;
+
